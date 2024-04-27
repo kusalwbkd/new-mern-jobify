4e417c6f0c77d52ad971ee242279748d7cd0a111
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2024-04-27 15:30:55 +0530

    done some changes

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ if(process.env.NODE_ENV=== 'development'){
     app.use(morgan('dev'))
 }
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser())
 app.use(express.json())
 
-
+//some chnages done
 
 app.get('/',(req,res)=>{
     res.send('hello world')
@@ -61,7 +61,7 @@ app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/users',authenticateUser,userRouter)
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
   });
 
 app.use('*',(req,res)=>{
